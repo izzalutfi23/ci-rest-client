feff36f9b47228d24177443db0ac298d9982d4d6
izzalutfi23 <izzalutfi045@gmail.com>
2019-11-29 17:30:16 +0700

    create view

---
+++ application/views/_header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<title>Home</title>
+</head>
+<body>
+	<h1>Home</h1>
+</body>
+</html>
